b50bf1c6c7c7d0e93cb6e0569977ca18554491c5
mattedwards <the.matt.edwards@googlemail.com>
2021-06-30 08:55:42 +0100

    add utils file with dateStringToDate function. Implemented in CsvFileReader to convert date string to Date object

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -10,8 +11,20 @@ export class CsvFileReader {
       encoding: "utf-8"
     })
     .split("\n")
-    .map((row: string): string[] => {
+    .map(
+      (row: string): string[] => {
       return row.split(",");
-    });
+      }
+    )
+    .map((row: string[]): any => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        
+      ]
+    })
   }
 }
